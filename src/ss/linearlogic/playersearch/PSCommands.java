906662fc02738a1f80d317054a7f46121a85baae
Jared Heath <silkrawnstudios@gmail.com>
2013-07-06 04:47:14 -0400

    [1.3] Add option to list all players ever on the server; reduce aliases

--- src/ss/linearlogic/playersearch/PSCommands.java
+++ src/ss/linearlogic/playersearch/PSCommands.java
@@ -8,41 +8,48 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class PSCommands implements CommandExecutor
-{
+public class PSCommands implements CommandExecutor {
+
 	private PlayerSearch plugin;
+	private final String prefix = ChatColor.GRAY + "[" + ChatColor.DARK_PURPLE + "PlayerSearch" + ChatColor.GRAY + "] ";
+
 	public PSCommands(PlayerSearch instance) {
 		plugin = instance;
 	}
-	
+
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if ((sender instanceof Player) && (!sender.hasPermission("playersearch.lookup")) && (!sender.hasPermission("playersearch.find")) && (!sender.hasPermission("playersearch.search"))) {
-			sender.sendMessage(ChatColor.GRAY + "[" + ChatColor.DARK_PURPLE + "PlayerSearch" + ChatColor.GRAY + "] " + ChatColor.RED + "Uh oh! " + ChatColor.GRAY + "You don't have permission to search for players.");
+		if ((sender instanceof Player) && (!sender.hasPermission("playersearch.lookup")) &&
+				(!sender.hasPermission("playersearch.find")) && (!sender.hasPermission("playersearch.search"))) {
+			sender.sendMessage(prefix + ChatColor.RED + "Uh oh! " + ChatColor.GRAY + "You don't have permission to " +
+					"search for players.");
 			return true;
 		}
 		if (args.length != 1) {
-			if (args.length == 0) {
-				sender.sendMessage(ChatColor.GRAY + "[" + ChatColor.DARK_PURPLE + "PlayerSearch" + ChatColor.GRAY + "] " + ChatColor.RED + "Too few arguments. " + ChatColor.GRAY + "Usage is /lookup <name>. The name doesn't need to be complete.");
-				return true;
-			}
-			sender.sendMessage(ChatColor.GRAY + "[" + ChatColor.DARK_PURPLE + "PlayerSearch" + ChatColor.GRAY + "] " + ChatColor.RED + "Too many arguments. " + ChatColor.GRAY + "Usage is /lookup <name>. The name doesn't need to be complete.");
+			sender.sendMessage(prefix + ChatColor.RED + "Too " + (args.length == 0 ? "few" : "many") + "arguments - " + 
+					ChatColor.GRAY + "Usage is /lookup <name>. The name doesn't need to be complete. Enter '*' as " +
+					"the name to list all players that have ever logged on to the server.");
 			return true;
 		}
+
 		ArrayList<String> onlinePlayers = new ArrayList<String>();
 		ArrayList<String> offlinePlayers = new ArrayList<String>();
 		String search = args[0];
-		for (Player player : plugin.getServer().getOnlinePlayers()) // search online players first, to make sure the offline player results aren't redundant
-			if (player.getName().toLowerCase().contains(search.toLowerCase()))
-				onlinePlayers.add(player.getName());
-		for (String playerName : plugin.totalPlayers) // search all players, and only record those that are offline
-			if ((playerName.toLowerCase().contains(search.toLowerCase())) && (!onlinePlayers.contains(playerName)))
-				offlinePlayers.add(playerName);
+		for (int i = 0; i < plugin.totalPlayers.size(); i++) {
+			String name = plugin.totalPlayers.get(i);
+			if (!search.equals("*") && !name.toLowerCase().contains(search.toLowerCase()))
+				continue;
+			if (plugin.getServer().getPlayer(name) != null)
+				onlinePlayers.add(name);
+			else
+				offlinePlayers.add(name);
+		}
 		int resultCount = 0;
-		if (!onlinePlayers.isEmpty())
-			resultCount += onlinePlayers.size();
-		if (!offlinePlayers.isEmpty())
-			resultCount += offlinePlayers.size();
-		sender.sendMessage(ChatColor.GRAY + "[" + ChatColor.DARK_PURPLE + "PlayerSearch" + ChatColor.GRAY + "] There are " + ChatColor.AQUA + resultCount + ChatColor.GRAY + " players whose names contain \"" + ChatColor.AQUA + args[0] + ChatColor.GRAY + "\":\n" + ChatColor.GREEN + "Online: " + ChatColor.GRAY + onlinePlayers.toString() + "\n" + ChatColor.RED + "Offline: " + ChatColor.GRAY + offlinePlayers.toString());
+		resultCount += onlinePlayers.size();
+		resultCount += offlinePlayers.size();
+		sender.sendMessage(prefix + "There are " + ChatColor.DARK_PURPLE + resultCount + ChatColor.GRAY + " players " +
+				"whose names contain \"" + ChatColor.AQUA + args[0] + ChatColor.GRAY + "\":\n" + ChatColor.GREEN +
+				"Online: " + ChatColor.GRAY + onlinePlayers.toString() + "\n" + ChatColor.RED + "Offline: " +
+				ChatColor.GRAY + offlinePlayers.toString());
 		return true;
 	}
 }
